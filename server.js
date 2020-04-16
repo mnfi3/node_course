ee977bf7745b3c55aeae05b0689e631e4414b8c6
mohsen340 <mohsenfarjami340@gmail.com>
2020-04-16 21:14:51 +0430

    add projects page

--- server.js
+++ server.js
@@ -55,6 +55,11 @@ app.get('/about', (req, res) => {
   })
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+  })
+});
 
 app.get('/bad', (req, res) => {
   res.send({
